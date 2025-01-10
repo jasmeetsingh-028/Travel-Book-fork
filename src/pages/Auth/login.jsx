b1c919d0e1410891c0941b4cfcc15850229e12cb
Sahil <sahilk64555@gmail.com>
2025-01-10 23:47:32 +0530

    device responsive

--- src/pages/Auth/login.jsx
+++ src/pages/Auth/login.jsx
@@ -1,93 +1,96 @@
 import React, { useState } from "react";
 import PasswordInput from "../../components/Input/PasswordInput";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosInstance";
 
 const Login = () => {
-
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [error,setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    
-    if(!validateEmail(email)){
+
+    if (!validateEmail(email)) {
       setError("Please enter a valid email address to get your travel book!");
       return;
     }
 
-    if(!password)
-    {
+    if (!password) {
       setError("Please enter the password!");
       return;
     }
 
     setError("");
-    
 
     // LOGIN API CALL WILL BE THERE.
-    try{
-      const response = await axiosInstance.post("/login",{
-        email:email,
-        password:password,
+    try {
+      const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
       });
 
-      if(response.data && response.data.accessToken)
-      {
+      if (response.data && response.data.accessToken) {
         localStorage.setItem("token", response.data.accessToken);
         navigate("/dashboard");
       }
-    }catch(error){
-      if(
-        error.response && 
+    } catch (error) {
+      if (
+        error.response &&
         error.response.data &&
         error.response.data.message
       ) {
         setError(error.response.data.message);
-      } else{
-        setError("An unexpected error occurred in Travel Book's Backend, Please try again.")
+      } else {
+        setError(
+          "An unexpected error occurred in Travel Book's Backend, Please try again."
+        );
       }
     }
-  }
+  };
 
   return (
     <div className="h-screen bg-cyan-50 overflow-hidden relative">
+      <div className="login-ui-box right-10 -top-40" />
+      <div className="login-ui-box " />
+      <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
 
-      <div className="login-ui-box right-10 -top-40"/>
-      <div className="login-ui-box "/>
-      <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2"/>
-
-      <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-        <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
+      <div className="container h-screen flex items-center justify-center px-5 sm:px-10 lg:px-20 mx-auto">
+        <div className="w-full sm:w-2/4 lg:w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
           <div>
-            <h4 className="text-5xl text-white font-semibold leading-[58px]"> 
+            <h4 className="text-3xl sm:text-4xl lg:text-5xl text-white font-semibold leading-[58px]">
               Capture Your <br /> Journeys
             </h4>
-            <p className="text-[15px] text-white leading-6 pr-7 mt-4">
-              Record your travel experiences and memories in your personal travel journey.
+            <p className="text-sm sm:text-base text-white leading-6 pr-7 mt-4">
+              Record your travel experiences and memories in your personal
+              travel journey.
             </p>
           </div>
-        </div>  
+        </div>
 
-        <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
+        <div className="w-full sm:w-2/4 lg:w-2/4 h-[75vh] bg-white rounded-r-lg relative p-5 sm:p-10 lg:p-16 shadow-lg shadow-cyan-200/20">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl font-semibold mb-7">Login</h4>
 
-            <input type="text" placeholder="Email" className="input-box" 
-            value={email}
-            onChange={({target})=>{
-              setEmail(target.value);
-            }}
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={({ target }) => {
+                setEmail(target.value);
+              }}
             />
 
-            <PasswordInput value={password}
-            onChange={({target})=>{
-              setPassword(target.value);
-            }} /> 
+            <PasswordInput
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+            />
 
             {error && <p className="text-red-600 text-xs pb-1">{error}</p>}
 
@@ -97,9 +100,13 @@ const Login = () => {
 
             <p className="text-xs text-slate-500 text-center my-4">Or</p>
 
-            <button type="submit" className="btn-primary btn-light" onClick={()=>{
-              navigate("/signUp");
-            }}>
+            <button
+              type="submit"
+              className="btn-primary btn-light"
+              onClick={() => {
+                navigate("/signUp");
+              }}
+            >
               CREATE ACCOUNT
             </button>
           </form>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
   }
 
   // Get all the travel stories
-  const getAllTravelStories = async ()=>{
+  const getAllTravelStories = async ()=> {
     try {
       const response = await axiosInstance.get("/get-all-stories");
       if(response.data && response.data.stories)
@@ -88,7 +88,6 @@ const Home = () => {
         "/update-is-favourite/" + storyId,
         {
           isFavourite: !storyData.isFavourite,
-
         }
       );
       if(response.data && response.data.story){
@@ -115,20 +114,19 @@ const Home = () => {
   const deleteTravelStory = async (data) => {
     const storyId = data._id;
 
-  try {
-    const response = await axiosInstance.delete("/delete-story/" + storyId);
+    try {
+      const response = await axiosInstance.delete("/delete-story/" + storyId);
 
-    if (response.data && !response.data.error) {
-      toast.error("Story Deleted Successfully");
+      if (response.data && !response.data.error) {
+        toast.error("Story Deleted Successfully");
 
-      setOpenViewModal((prevState) => ({ ...prevState, isShown: false }));
+        setOpenViewModal((prevState) => ({ ...prevState, isShown: false }));
 
-      getAllTravelStories();
+        getAllTravelStories();
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
     }
-  } catch (error) {
-    // Handle unexpected errors
-    console.log("An unexpected error occurred. Please try again.");
-  }
   }
 
   // To search a story
@@ -144,21 +142,18 @@ const Home = () => {
         setFilterType("search");
         setAllStories(response.data.stories);
       }
-  
-     
     } catch (error) {
-      // Handle unexpected errors
       console.log("An unexpected error occurred. Please try again.");
     }
   }
 
-  const handleClearSearch = () =>{
+  const handleClearSearch = () => {
     setFilterType("");
     getAllTravelStories();
   }
 
   // handle filter travel story by date range
-  const filterStoriesByDate = async (day) =>{
+  const filterStoriesByDate = async (day) => {
     try {
       const startDate = day.from ? moment(day.from).valueOf() : null;
       const endDate = day.to ? moment(day.to).valueOf() : null;
@@ -170,10 +165,10 @@ const Home = () => {
         });
 
         if(response.data && response.data.stories)
-          {
-            setFilterType("date");
-            setAllStories(response.data.stories);
-          }
+        {
+          setFilterType("date");
+          setAllStories(response.data.stories);
+        }
       }
     } catch (error) {
       console.log("An unexpected error occured in filtering stories by date!");
@@ -182,121 +177,114 @@ const Home = () => {
   };
 
   // handle day range selector
-  const handleDayClick =(day) =>{
+  const handleDayClick = (day) => {
     setDataRange(day);
     filterStoriesByDate(day);
   }
 
-  const resetFilter = () =>{
+  const resetFilter = () => {
     setDataRange({from:null,to:null});
     setFilterType("");
     getAllTravelStories();
   }
   
-useEffect(() => {
-  getAllTravelStories();
-  getUserInfo();
-
-  return () => {
-    
-  }
-}, [])
+  useEffect(() => {
+    getAllTravelStories();
+    getUserInfo();
+  }, [])
 
   return (
     <>
-      <Navbar userInfo = {userInfo} searchQuery={searchQuery} setSearchQuery={setSearchQuery} onSearchNote={onSearchStory} handleClearSearch={handleClearSearch}/>
+      <Navbar userInfo={userInfo} searchQuery={searchQuery} setSearchQuery={setSearchQuery} onSearchNote={onSearchStory} handleClearSearch={handleClearSearch} />
 
       <div className='container mx-auto py-10'>
 
-        <FilterInfoTitle filterType={filterType} filterDates={dataRange} onClear = {()=>{
-          resetFilter();
-        }} />
+        <FilterInfoTitle filterType={filterType} filterDates={dataRange} onClear={resetFilter} />
 
-        <div className='flex gap-7'>
+        <div className='flex flex-col sm:flex-row gap-7'>
           <div className='flex-1'>
-          {allStories.length > 0 ? (
-        <div className="grid grid-cols-2 gap-4">
-          {allStories.map((item) => (
-            <TravelStoryCard
-            key={item._id} 
-            imgUrl = {item.imageUrl}
-            title = {item.title}
-            story = {item.story}
-            date = {item.visitedDate}
-            visitedLocation = {item.visitedLocation}
-            isFavourite = {item.isFavourite}
-            // onEdit = {()=> handleEdit(item)}
-            onClick={() => handleViewStory(item)}
-            onFavouriteClick = {()=> updateIsFavourite(item)}
-            />
-          ))}
-        </div>
-      ) : (
-        <EmptyCard imgSrc={getEmptyImg(filterType)} message={getEmptyCardMessage(filterType)}  />
-      )}
+            {allStories.length > 0 ? (
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                {allStories.map((item) => (
+                  <TravelStoryCard
+                    key={item._id}
+                    imgUrl={item.imageUrl}
+                    title={item.title}
+                    story={item.story}
+                    date={item.visitedDate}
+                    visitedLocation={item.visitedLocation}
+                    isFavourite={item.isFavourite}
+                    onClick={() => handleViewStory(item)}
+                    onFavouriteClick={() => updateIsFavourite(item)}
+                  />
+                ))}
+              </div>
+            ) : (
+              <EmptyCard imgSrc={getEmptyImg(filterType)} message={getEmptyCardMessage(filterType)} />
+            )}
           </div>
-          
-            <div className='w-[320px]'>
-              <div className='bg-white border border-slate-200 shadow-lg shadow-slate-200/60 rounded-lg'>
-                <div className='p-3'>
-                  <DayPicker captionLayout='dropdown-buttons' mode="range" selected = {dataRange} onSelect ={handleDayClick} pagedNavigation/>
-                </div>
+
+          <div className='w-full sm:w-[320px]'>
+            <div className='bg-white border border-slate-200 shadow-lg shadow-slate-200/60 rounded-lg'>
+              <div className='p-3'>
+                <DayPicker captionLayout='dropdown-buttons' mode="range" selected={dataRange} onSelect={handleDayClick} pagedNavigation />
               </div>
             </div>
-      </div>
+          </div>
+        </div>
       </div>
 
       {/* Add and edit travel story */}
       <Modal isOpen={openAddEditModal.isShown}
-      onRequestClose={()=>{}}
-      style={{
-        overlay:{
-          backgroundColor:"rgba(0,0,0,0.2)",
-          zIndex:999,
-        },
-      }}
-      appElement={document.getElementById("root")}
-      className="model-box">
-        <AddEditTravelStory type={openAddEditModal.type}
-        storyInfo={openAddEditModal.data}
-        onClose={()=>{
-          setOpenAddEditModal({isShown:false,type:"add",data:null});
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+            zIndex: 999,
+          },
         }}
-        getAllTravelStories={getAllTravelStories}
+        appElement={document.getElementById("root")}
+        className="model-box">
+        <AddEditTravelStory type={openAddEditModal.type}
+          storyInfo={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+          getAllTravelStories={getAllTravelStories}
         />
       </Modal>
-      
+
       {/* view travel story */}
       <Modal isOpen={openViewModal.isShown}
-      onRequestClose={()=>{}}
-      style={{
-        overlay:{
-          backgroundColor:"rgba(0,0,0,0.2)",
-          zIndex:999,
-        },
-      }}
-      appElement={document.getElementById("root")}
-      className="model-box">
-        <ViewTravelStory
-        storyInfo={openViewModal.data || null} onClose = {()=>{
-          setOpenViewModal((prevState) => ({...prevState,isShown:false}));
-        }} onEditClick = {()=>{
-          setOpenViewModal((prevState) => ({...prevState,isShown:false}));
-          handleEdit(openViewModal.data || null)
-        }} onDeleteClick = {()=>{
-          deleteTravelStory(openViewModal.data || null);
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+            zIndex: 999,
+          },
         }}
+        appElement={document.getElementById("root")}
+        className="model-box">
+        <ViewTravelStory
+          storyInfo={openViewModal.data || null} onClose={() => {
+            setOpenViewModal((prevState) => ({ ...prevState, isShown: false }));
+          }} onEditClick={() => {
+            setOpenViewModal((prevState) => ({ ...prevState, isShown: false }));
+            handleEdit(openViewModal.data || null)
+          }} onDeleteClick={() => {
+            deleteTravelStory(openViewModal.data || null);
+          }}
         />
       </Modal>
 
-      <button className='w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-cyan-400 fixed right-10 bottom-10' onClick={()=>{
-        setOpenAddEditModal({isShown:true,type:"add",data:null})
-      }}>
-        <MdAdd className="text-[32px] text-white"/>
+      <button className='w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-cyan-400 fixed right-10 bottom-10'>
+        <MdAdd className="text-[32px] text-white" onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null })
+        }} />
       </button>
 
-      <ToastContainer/>
-      </>
+      <ToastContainer />
+    </>
   )
 }
 
