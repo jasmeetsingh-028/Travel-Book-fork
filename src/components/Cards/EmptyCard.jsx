18061d7470727a67b403ad5ce43e7495282d2c3e
Sahil <118194056+Sahilll94@users.noreply.github.com>
2025-01-11 17:29:25 +0530

    Update EmptyCard.jsx

--- src/components/Cards/EmptyCard.jsx
+++ src/components/Cards/EmptyCard.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 const EmptyCard = ({ imgSrc, message }) => {
     return (
-       <div className="flex flex-col items-center justify-center mt-10 md:mt-20 mx-4 md:ml-96"> {/* Adjusted margins */}
-  <img src={imgSrc} alt="No notes" className="w-20 md:w-24" />
-  <p className="w-full md:w-1/2 text-base md:text-lg font-bold text-slate-700 text-center leading-7 mt-6 md:mt-10">
-    {message}
-  </p>
-</div>
+        <div className="flex flex-col items-center justify-center mt-20 ml-96">
+            <img src={imgSrc} alt="No notes" className="w-24" />
+
+            <p className="w-1/2 text-lg font-bold text-slate-700 text-center leading-7 mt-10">
+                {message}
+            </p>
+        </div>
     );
 };
 
 export default EmptyCard;
-
-
-
