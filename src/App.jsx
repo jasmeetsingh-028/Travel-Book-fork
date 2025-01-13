a98dac4e0f02a46a3b5b078725a1f3b297084129
Sahil <sahilk64555@gmail.com>
2025-01-13 21:22:37 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home.jsx';
 import Hero from './../src/pages/hero/Hero.jsx';
 import Mistake from './pages/mistake.jsx'; 
 import StoryDetails from './../src/pages/home/StoryDetails.jsx';
+import DownloadStory from './pages/home/DownloadStoryPage.jsx'; // Import the new component
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/signup" exact element={<SignUp />} />
           <Route path="*" exact element={<Mistake />} /> {/* 404 not found page */}
           <Route path="/story/:id" element={<StoryDetails />} />
+          <Route path="/download-story/:id" element={<DownloadStory />} /> {/* Add the download story route */}
         </Routes>
       </Router>
     </div>
