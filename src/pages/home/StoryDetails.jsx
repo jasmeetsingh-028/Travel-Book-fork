202e74e3aa06055bfdfd7f5c944e1d96e524d020
Sahil <118194056+Sahilll94@users.noreply.github.com>
2025-01-15 06:45:20 +0530

    Update StoryDetails.jsx

--- src/pages/home/StoryDetails.jsx
+++ src/pages/home/StoryDetails.jsx
@@ -239,3 +239,31 @@ const ErrorMessage = styled.div`
   font-size: 1.5rem;
   color: red;
 `;
+
+const Loading = styled.div`
+  font-size: 1.5rem;
+  color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  ::after {
+    content: "";
+    border: 4px solid transparent;
+    border-top: 4px solid #4caf50;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
