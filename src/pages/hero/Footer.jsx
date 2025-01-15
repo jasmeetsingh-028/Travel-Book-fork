bb4fe62dcd501e637eb2f317479d057dff57f532
Sahil <118194056+Sahilll94@users.noreply.github.com>
2025-01-16 00:23:31 +0530

    Update Footer.jsx

--- src/pages/hero/Footer.jsx
+++ src/pages/hero/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
                     <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                        <a href = "https://travelbook.sahilportfolio.me/"
+                        <a href = "https://travelbook.sahilportfolio.me/">
                         <img className="w-40 h-30" src={logo} alt="" />
                         </a>
 
