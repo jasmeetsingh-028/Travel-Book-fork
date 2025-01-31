0185fb7852e4cb4d6c4c68d7e60f240ba08ec437
Sahil <118194056+Sahilll94@users.noreply.github.com>
2025-01-31 21:05:47 +0530

    Update Hero.jsx

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -323,14 +323,14 @@ const Hero = () => {
             <span className="relative flex justify-center">
                 <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
 
-{/*                 <span className="relative z-10 bg-white px-6">
+                <span className="relative z-10 bg-white px-6">
                     Oh, So do you like the design?
                 </span>
-            </span> */}
+            </span>
 
-{/*             <section id="stats">
+            <section id="stats">
                 <Stats />
-            </section> */}
+            </section>
 
             <span className="relative flex justify-center">
                 <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
