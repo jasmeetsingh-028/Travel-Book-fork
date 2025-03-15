31182c709fb4442fb66ebb7ed7b24f0b3db29212
Sahil <118194056+Sahilll94@users.noreply.github.com>
2025-03-15 12:48:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "react-day-picker/style.css"
-import './scrollbar.css'; // Import the scrollbar styles
+import './scrollbar.css'; 
 
 
 createRoot(document.getElementById('root')).render(
